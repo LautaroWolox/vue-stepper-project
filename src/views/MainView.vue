2d4fef800d9c28078211fc3eed099fbcdfc93642
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-06-29 08:23:54 -0300

    Show animated FM logo on welcome screen

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -3,10 +3,8 @@
     <AppMenu @load-module="loadModule" />
     
     <div class="content-area" style="padding: 20px; max-width: 100%; margin: 0 auto;">
-      <div v-if="activeModule === 'none'" style="text-align: center; margin-top: 50px; color: #90a4ae;">
-        <span class="material-icons" style="font-size: 80px; opacity: 0.5;">widgets</span>
-        <h2>Bienvenido al Field Manager</h2>
-        <p>Selecciona un módulo desde el menú superior.</p>
+      <div v-if="activeModule === 'none'" class="welcome-fm-screen">
+        <FmSplash />
       </div>
       
       <SearchAccordion v-if="activeModule === 'search-classic'" />
@@ -24,6 +22,7 @@
 <script setup>
 import { ref } from 'vue'
 import AppMenu from '../components/menu/AppMenu.vue'
+import FmSplash from '../components/brand/FmSplash.vue'
 import SearchAccordion from '../components/accordion/SearchAccordion.vue'
 import GestionMateriales from '../components/materiales/GestionMateriales.vue'
 import ConfigJobtype from '../components/certificacion/ConfigJobtype.vue'
@@ -38,3 +37,12 @@ const loadModule = (action) => {
   activeModule.value = action
 }
 </script>
+
+<style scoped>
+.welcome-fm-screen {
+  min-height: calc(100vh - 95px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
