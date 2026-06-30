2d8122231de6a3aae83287d4503066fefefb81f7
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-06-30 02:44:46 -0300

    Add shared fm icon component

---
+++ src/shared/fm-icons/FmIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <span class="material-icons fm-icon">{{ name }}</span>
+</template>
+
+<script setup>
+defineProps({
+  name: { type: String, required: true }
+})
+</script>
+
+<style scoped>
+.fm-icon{font-size:18px;line-height:1;vertical-align:middle}.fm-icon:hover{color:#00a9bd}
+</style>
