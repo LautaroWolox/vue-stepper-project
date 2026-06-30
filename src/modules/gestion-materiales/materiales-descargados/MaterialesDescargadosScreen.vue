d07ab9d0cbfd7ef110c6ee8ff6d602045acc16ec
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-06-30 02:48:17 -0300

    Add materiales descargados module screen

---
+++ src/modules/gestion-materiales/materiales-descargados/MaterialesDescargadosScreen.vue
@@ -0,0 +1,7 @@
+<template>
+  <GestionMateriales title="Materiales Descargados" type="DESC" />
+</template>
+
+<script setup>
+import GestionMateriales from '../../../components/materiales/GestionMateriales.vue'
+</script>
