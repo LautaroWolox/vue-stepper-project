751fde4b793f5c9a085b4554755dfc6f2035c0eb
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-06-29 23:00:31 -0300

    Wire Reporte SAS module

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -18,6 +18,7 @@
       <ConsultarNotaCredito v-if="activeModule === 'consultar-nota-credito'" />
       <ConsultarNotaDebito v-if="activeModule === 'consultar-nota-debito'" />
       <RegistroOtFallidas v-if="activeModule === 'registro-ots-fallidas'" />
+      <ReporteSas v-if="activeModule === 'reporte-sas'" />
     </div>
   </div>
 </template>
@@ -37,6 +38,7 @@ import ConsultarActas from '../components/certificacion/ConsultarActas.vue'
 import ConsultarNotaCredito from '../components/certificacion/ConsultarNotaCredito.vue'
 import ConsultarNotaDebito from '../components/certificacion/ConsultarNotaDebito.vue'
 import RegistroOtFallidas from '../components/certificacion/RegistroOtFallidas.vue'
+import ReporteSas from '../components/reportes/ReporteSas.vue'
 
 const activeModule = ref('none')
 
