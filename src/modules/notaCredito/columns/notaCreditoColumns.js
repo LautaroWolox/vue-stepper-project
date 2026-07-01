f053e2cbe83af7f762995d7f53d6dc40b57ed6f0
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-01 12:55:51 -0300

    Add nota credito columns

---
+++ src/modules/notaCredito/columns/notaCreditoColumns.js
@@ -0,0 +1,17 @@
+export const notaCreditoColumns = [
+  { field: 'nro_nota', label: 'NRO_NOTA', width: 130, minWidth: 70 },
+  { field: 'acta', label: 'NUMERO DE ACTA', width: 135, minWidth: 75 },
+  { field: 'estado', label: 'ESTADO_NOTA', width: 125, minWidth: 70 },
+  { field: 'periodo', label: 'PERIODO', width: 145, minWidth: 80 },
+  { field: 'anio', label: 'AÑO', width: 90, minWidth: 55 },
+  { field: 'f_crea', label: 'FECHA_CREACION', width: 150, minWidth: 80 },
+  { field: 'f_cierre', label: 'FECHA_CIERRE_NC', width: 150, minWidth: 80 },
+  { field: 'contra', label: 'CONTRATISTA', width: 165, minWidth: 90 },
+  { field: 'pais', label: 'PAIS', width: 120, minWidth: 60 },
+  { field: 'prov', label: 'PROVINCIA', width: 135, minWidth: 75 },
+  { field: 'reg', label: 'REGION', width: 125, minWidth: 70 },
+  { field: 'tipo', label: 'TIPO_CONTRATO', width: 135, minWidth: 80 },
+  { field: 'soc', label: 'SOCIEDAD', width: 155, minWidth: 80 },
+  { field: 'usu', label: 'USUARIO_CIERRE', width: 135, minWidth: 80 },
+  { field: 'val', label: 'VALORACION_ACTA', width: 135, minWidth: 75 }
+]
