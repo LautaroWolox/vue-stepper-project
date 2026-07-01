dfa9f08addae4a8d88e190e6095343cab9a783b2
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-01 12:55:07 -0300

    Import shared project theme css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
+import './assets/css/nuestros.css'
 import './assets/css/fallidas-fix-filters.css'
 import './assets/css/fallidas-fix-grid.css'
 import './assets/css/fallidas-fix-empty.css'
