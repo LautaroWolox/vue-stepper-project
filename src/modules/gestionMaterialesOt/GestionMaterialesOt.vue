0d64f9b211b3015db60447ee4ce56864180d6292
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-01 12:52:13 -0300

    Add materiales OT module

---
+++ src/modules/gestionMaterialesOt/GestionMaterialesOt.vue
@@ -0,0 +1,7 @@
+<template>
+  <GestionMaterialesOtScreen />
+</template>
+
+<script setup>
+import GestionMaterialesOtScreen from '../gestion-materiales/gestion-materiales-ot/GestionMaterialesOtScreen.vue'
+</script>
