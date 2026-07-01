17432f751e01672a8025b8cf24356097d8204cd8
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-01 12:58:08 -0300

    Add CT module types

---
+++ src/modules/registroOtsFallidasCT/types.js
@@ -0,0 +1,9 @@
+export const CT_FALLIDAS_MODULE = 'registro-ots-fallidas-certificacion'
+
+export const CT_FALLIDAS_ACTIONS = {
+  SEARCH: 'search',
+  EXPORT: 'export',
+  EXCLUDE: 'exclude',
+  PROCESS: 'process',
+  INCLUDE: 'include'
+}
