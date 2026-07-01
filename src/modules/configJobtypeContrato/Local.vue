5842a029efbb84991016e22a9d2b9565c310dff6
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-01 15:03:17 -0300

    Add local jobtype module

---
+++ src/modules/configJobtypeContrato/Local.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="local-jobtype-module"><component :is="modulo" /></div>
+</template>
+
+<script setup>
+import { shallowRef } from 'vue'
+import Pantalla from '../../components/certificacion/ConfigJobtype.vue'
+const modulo = shallowRef(Pantalla)
+</script>
