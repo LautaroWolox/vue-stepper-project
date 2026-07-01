2a65558afe9ca26c7555d02b9be256e32c2ae7d3
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-01 12:50:57 -0300

    Add CMO actividad module entrypoint

---
+++ src/modules/configCmoActividad/ConfigCmoActividad.vue
@@ -0,0 +1,7 @@
+<template>
+  <ConfigCmoActividadScreen />
+</template>
+
+<script setup>
+import ConfigCmoActividadScreen from '../actas/config-cmo-actividad/ConfigCmoActividadScreen.vue'
+</script>
