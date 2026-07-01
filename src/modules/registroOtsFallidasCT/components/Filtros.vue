6ea9b72a2727c544ddcdf907a1f4ecfcb9ab9d21
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-01 14:41:57 -0300

    Move CT filters into local module

--- src/modules/registroOtsFallidasCT/components/Filtros.vue
+++ src/modules/registroOtsFallidasCT/components/Filtros.vue
@@ -1,7 +1,29 @@
 <template>
-  <FallidasFilters v-bind="$attrs" />
+  <section class="fallidas-accordion">
+    <button class="fallidas-header" type="button" @click="$emit('toggle')">
+      <span>FILTROS DE BUSQUEDA</span>
+      <span>{{ open ? '-' : '+' }}</span>
+    </button>
+    <div v-show="open" class="fallidas-filter-panel">
+      <div class="fallidas-filters-grid">
+        <label><span>Nro. OT</span><input v-model.trim="model.nroOt" class="fallidas-input" /></label>
+        <label><span>Fecha Cierre OT Desde</span><DatePicker v-model="model.fechaDesde" placeholder="Seleccionar fecha" /></label>
+        <label><span>Fecha Cierre OT Hasta</span><DatePicker v-model="model.fechaHasta" placeholder="Seleccionar fecha" /></label>
+        <label><span>Contratista</span><FmTurquoiseSelect v-model="model.contratista" :options="contratistaOptions" class="fallidas-filter-select" /></label>
+        <label><span>Descripcion Error</span><input v-model.trim="model.descripcionError" class="fallidas-input" /></label>
+        <label><span>Excluida</span><FmTurquoiseSelect v-model="model.excluida" :options="excluidaOptions" class="fallidas-filter-select fallidas-small-select" /></label>
+        <label><span>Pais</span><FmTurquoiseSelect v-model="model.pais" :options="paisOptions" class="fallidas-filter-select fallidas-small-select" /></label>
+      </div>
+      <div class="fallidas-center-actions">
+        <button class="fallidas-btn-primary" :disabled="loading" @click="$emit('search')"><span class="material-icons">search</span><span>BUSCAR</span></button>
+        <button class="fallidas-btn-secondary" :disabled="loading" @click="$emit('clear')"><span class="material-icons">sort</span><span>LIMPIAR</span></button>
+      </div>
+    </div>
+  </section>
 </template>
-
 <script setup>
-import FallidasFilters from '../../actas/registro-ot-fallidas/FallidasFilters.vue'
+import FmTurquoiseSelect from '../../../components/shared/FmTurquoiseSelect.vue'
+import DatePicker from './DatePicker.vue'
+defineProps({ open: Boolean, loading: Boolean, model: Object, contratistaOptions: Array, excluidaOptions: Array, paisOptions: Array })
+defineEmits(['toggle', 'search', 'clear'])
 </script>
