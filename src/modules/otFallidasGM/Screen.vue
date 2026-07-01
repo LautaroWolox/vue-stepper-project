8fef5be29d03b85bb460d33b4e8e4ed821cda0b0
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-01 15:01:29 -0300

    Add local GM fallidas screen

---
+++ src/modules/otFallidasGM/Screen.vue
@@ -0,0 +1,7 @@
+<template>
+  <GestionMateriales title="Registro de OTs Fallidas" type="OT" />
+</template>
+
+<script setup>
+import GestionMateriales from '../../components/materiales/GestionMateriales.vue'
+</script>
