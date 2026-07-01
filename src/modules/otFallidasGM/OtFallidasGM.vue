a436972a3133267018c0a1b16564f9b55ec6b3f1
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-01 12:52:33 -0300

    Add materiales failed OTs module

---
+++ src/modules/otFallidasGM/OtFallidasGM.vue
@@ -0,0 +1,7 @@
+<template>
+  <RegistroOtsFallidasMaterialesScreen />
+</template>
+
+<script setup>
+import RegistroOtsFallidasMaterialesScreen from '../gestion-materiales/registro-ots-fallidas-materiales/RegistroOtsFallidasMaterialesScreen.vue'
+</script>
