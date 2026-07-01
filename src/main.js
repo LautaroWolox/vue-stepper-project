1e53b591e7304b7c0ebc6fd83d47fd8ac1a9cbd1
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-01 02:02:24 -0300

    Import validacion redes button overrides

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router.js'
 import './assets/css/fallidas-fix-filters.css'
 import './assets/css/fallidas-fix-grid.css'
 import './assets/css/fallidas-fix-empty.css'
+import './assets/css/validacion-redes-buttons.css'
 
 const app = createApp(App)
 app.use(router)
