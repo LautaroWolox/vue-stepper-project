2aca91def1422633179fd2fdd6bbf506f0b43bdb
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-01 15:41:16 -0300

    Remove old GM fallidas wrapper

--- src/modules/gestion-materiales/registro-ots-fallidas-materiales/RegistroOtsFallidasMaterialesScreen.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <GestionMateriales title="Registro de OTs Fallidas" type="OT" />
-</template>
-
-<script setup>
-import GestionMateriales from '../../../components/materiales/GestionMateriales.vue'
-</script>
